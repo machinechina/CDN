f60b1792422702cbc1398f53238c8fbd27f888f4
majun <majun@32d91889-fa02-9e4d-b8fd-8fc4873ef940>
2016-11-02 02:08:27 +0000

    Add Standalone Test

---
+++ 研发/CDN/CDN.Test.Standalone/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDN.Test.Standalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File Url:");
+            var url = Console.ReadLine();
+
+            Console.WriteLine("How many times:");
+            var times = int.Parse(Console.ReadLine());
+
+            var timePerParse = Stopwatch.StartNew();
+            Parallel.For(0, times, i =>
+            {
+                var path = Path.Combine("C:\\TestDownload", i.ToString());
+                Directory.CreateDirectory(path);
+                using (var webClient = new WebClient())
+                    webClient.DownloadFile(url, Path.Combine(path, "FILE"));
+            });
+            timePerParse.Stop();
+            Console.WriteLine("Finished,Total time:" + timePerParse.Elapsed.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
